d7c07132f8603b068fa82258e7ef95b2eac1d654
Boithlomolo <Boithlomolo@gmail.com>
2024-11-27 13:18:05 +0200

    Product Page

--- src/components/SelectCategory.jsx
+++ src/components/SelectCategory.jsx
@@ -3,8 +3,9 @@ import React from 'react'
 const SelectCategory = (select) => {
   return (
     <select>
-        <option value="All">All Categories</option>
-        <option value="clothing">Clothing</option>
+        <option value="All">All Clothing</option>
+        <option value="clothing">T-shirts</option>
+        <option value="clothing">Pants</option>
         <option value="shoes">Shoes</option>
         <option value="accessories">Accessories</option>
         <option value="hats">Hats</option>
